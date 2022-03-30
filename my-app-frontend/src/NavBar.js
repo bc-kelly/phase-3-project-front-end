e86ea5f0df3687a62176ac8b81d4496c7b6e8ef5
mckaymatt3 <mckaymatt@outlook.com>
2022-03-30 13:22:14 -0400

    Updated styling before the home page

--- my-app-frontend/src/NavBar.js
+++ my-app-frontend/src/NavBar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import { GiMountains } from "react-icons/gi";
+import { FaMountain } from "react-icons/fa";
+
 
 function NavBar() {
     return (
         <nav className="navbar">
                 <div className="navbar-header">
                 <div className="navbar-homepage">
-                    <h1>Air BnB But Betta</h1>
+                    <h1> Air BnB But Betta </h1>
+                    {/* <h3> <FaMountain /> <FaMountain /> <FaMountain /> </h3> */}
+                    <h3> <GiMountains /> <GiMountains /> <GiMountains /> </h3>
+
                 </div>
                 <div className="navbar-links">
                     <NavLink exact to="/">Home</NavLink>
