d1bc7ce1e836651125c20905ed442de5a1621883
mckaymatt3 <mckaymatt@outlook.com>
2022-03-31 12:13:01 -0400

    Updated styling Thursday before lunch

--- my-app-frontend/src/Home.js
+++ my-app-frontend/src/Home.js
@@ -5,8 +5,10 @@ function Home() {
     return ( 
         <div>
             <h1 className="home-page">Welcome to AirBnB But Betta</h1>
-            <h3 className="hp-mountains"> <GiMountains /> <GiMountains /> <GiMountains /> </h3>
+            <h3 className="hp-mountains"> <GiMountains size={100}/> <GiMountains size={100}/> <GiMountains size={100}/> </h3>
             <p className="future"> The future of stays</p>
+            <p className='booking'> Give us a call to book!</p>
+            <a className="lefty-call" href="https://www.youtube.com/watch?v=U4SmvlRmdsY"> Righty calls only - no lefty calls... </a>
         </div> 
     );
 }
