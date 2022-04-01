d62b7a01b02697621ac28e589107ff7327e99bc1
Bridget Kelly <bckelly197@gmail.com>
2022-04-01 10:04:03 -0400

    added filter to stays

--- my-app-frontend/src/HouseList.js
+++ my-app-frontend/src/HouseList.js
@@ -1,29 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import HouseCard from './HouseCard'
+
 // import { FcGlobe } from "react-icons/fc";
 
 function HouseList({houses, handleClickHouse}) {
-    // const [searchText, setSearchText] = useState("")
+    const [searchText, setSearchText] = useState("")
 
-    // const filterAllHouses = houses.filter(function (house){
-    //     return house.location.toLowerCase().includes(searchText.toLowerCase()) 
-    //     || house.content.toLowerCase().includes(searchText.toLowerCase())
-    //     || house.description.toLowerCase().includes(searchText.toLowerCase())
-    //   })
+    const filterAllHouses = houses.filter(function (house){
+        return house.location.toLowerCase().includes(searchText.toLowerCase()) 
+        || house.description.toLowerCase().includes(searchText.toLowerCase())
+      })
 
 
-    // function handleSearchText (event) {
-    //     console.log(event.target.value)
-    //     setSearchText(event.target.value)
-    //}
-    const showHouses = houses.map((house) => {
-       
+    function handleSearchText (event) {
+        console.log(event.target.value)
+        setSearchText(event.target.value)
+    }
+    // const showHouses = houses.map((house) => {
+        const showHouses = filterAllHouses.map((house) => {
         return (
             <HouseCard 
                 key={house.id}
                 house={house}
                 handleClickHouse={handleClickHouse}
-                
             />
         )
     })
@@ -38,7 +37,9 @@ function HouseList({houses, handleClickHouse}) {
             <h3 className="welcome-description">
                 Check out the lovely stays at <em>Air BnB But Betta!</em>
             </h3>
-
+            <div className="travel-list-search-div">
+            <   input className="travel-list-search" onChange={handleSearchText} value={searchText} id="search" type="text" placeholder="Search..."></input>
+            </div>
             <div className="houses-container">
                 {showHouses}
             </div>
